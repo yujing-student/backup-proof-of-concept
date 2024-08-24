355088dcc1c7049568e13b9b4b2284f7fc78730c
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-08-24 13:37:08 +0200

    aanpassingen gemaakt waardoor de server.js hetzelfde zou moeten zijn met de profilecard

--- api/server.js
+++ api/server.js
@@ -1,29 +1,31 @@
-// Importeerd npm pakket express uit de node_modules map
-import express from 'express'
-// Importeerd de zelfgemaakte functie fetchJson uit de ./helpers map
-import fetchJson from './helpers/fetch-json.js'
+
+// Importeer het npm pakket express uit de node_modules map
+// import express from 'express'
+// import path from 'path'
+async function fetchJson(url, payload = {}) {
+    try{//de reden voor een try and catch is zotat er een betere error adhandeling is en dat ik de error in de console log kan zien
+        //en de then als die niet reageert dan gaat die automatisch naar de catch
+        return await fetch(url, payload)
+            .then((response) => response.json())//reageeer op de aanroep en pas de informatie aan aar een json
+
+    }catch (error){
+        console.error('Error:', error);
+    }
+
+}
+const path = require('path')
+const express = require('express')
+// Import the path module for dirname function
+// Importeer de zelfgemaakte functie fetchJson uit de/helpers map
 
 const app = express()
 
-const audioUrl = 'http://25683.live.streamtheworld.com/BNR_BUSINESS_BEATS.mp3';
-// Stel ejs in als template engine
 app.set('view engine', 'ejs')
-// Stel de map met ejs templates in
-app.set('views', './views')
+app.set('views', path.join(__dirname, '../views'))
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));//deze regel code gebruiken vanwege middelware zodat de data leesbaar gemaakt word
 
-// Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
-
-// Zorg dat werken met request data makkelijker wordt
-app.use(express.urlencoded({extended: true}))
-// Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8004)
-
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-    // Toon een bericht in de console en geef het poortnummer door
-    console.log(`Application started on http://localhost:${app.get('port')}`)
-})
 
 
 app.get('/', (request, response) => {
@@ -37,5 +39,11 @@ app.get('/', (request, response) => {
 })
 
 
-module.exports = app;
+app.set('port', process.env.PORT || 8004)
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+    // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+module.exports = app
 
